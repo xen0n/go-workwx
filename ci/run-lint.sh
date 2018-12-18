cfb8313d98764e4669f686ac74715de107fd0a04
Wang Xuerui <wangxuerui@qiniu.com>
2018-12-18 22:57:05 +0800

    chore: set up travis

---
+++ ci/run-lint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+TOP="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+cd "${TOP}"
+
+
+die() {
+    echo fatal: lint failed: $@ >&2
+    exit 1
+}
+
+
+# gofmt conformance check
+check_gofmt() {
+    # record the list of non-conforming source files
+    local output="$(gofmt -s -l $@)"
+    if [[ "x${output}" != "x" ]]; then
+        # report the offending files then die
+        die "the following files violate gofmt -s style: ${output}"
+    fi
+}
+
+check_gofmt .
+
+
+# staticcheck for known problems
+staticcheck . || die "staticcheck reported error(s)"
