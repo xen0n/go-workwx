225e4263f075a6cd9b57fa1d0ec04e4bce86a9f6
Wang Xuerui <wangxuerui@qiniu.com>
2018-12-19 09:35:01 +0800

    docs: the "traits" are not marker traits because they have methods

--- traits.go
+++ traits.go
@@ -4,13 +4,13 @@ import (
 	"net/url"
 )
 
-// urlValuer 可转化为 url.Values 类型的 marker trait
+// urlValuer 可转化为 url.Values 类型的 trait
 type urlValuer interface {
 	// IntoURLValues 转换为 url.Values 类型
 	IntoURLValues() url.Values
 }
 
-// bodyer 可转化为 API 请求体的 marker trait
+// bodyer 可转化为 API 请求体的 trait
 type bodyer interface {
 	// IntoBody 转换为请求体的 []byte 类型
 	IntoBody() []byte
