9951f96f5506538d9f91e318a44c280685868045
Wang Xuerui <wangxuerui@qiniu.com>
2018-12-20 11:45:56 +0800

    docs: add godoc examples

---
+++ example_test.go
@@ -0,0 +1,60 @@
+package workwx_test
+
+import (
+	"github.com/xen0n/go-workwx"
+)
+
+func ExampleWorkwx() {
+	corpID := "your_corpid"
+	corpSecret := "your_corpsecret"
+	agentID := int64(1234567)
+
+	client := workwx.New(corpID)
+
+	app := client.WithApp(corpSecret, agentID)
+	app.SpawnAccessTokenRefresher()
+
+	// see other examples for more details
+}
+
+func ExampleWorkwxApp_SendTextMessage() {
+	corpID := "your_corpid"
+	corpSecret := "your_corpsecret"
+	agentID := int64(1234567)
+
+	client := workwx.New(corpID)
+
+	app := client.WithApp(corpSecret, agentID)
+	// preferably do this at app initialization
+	app.SpawnAccessTokenRefresher()
+
+	// send to user(s)
+	to1 := workwx.Recipient{
+		UserIDs: []string{"testuser"},
+	}
+	_ = app.SendTextMessage(&to1, "send to user(s)", false)
+
+	// "safe" message
+	to2 := workwx.Recipient{
+		UserIDs: []string{"testuser"},
+	}
+	_ = app.SendTextMessage(&to2, "safe message", true)
+
+	// send to party(parties)
+	to3 := workwx.Recipient{
+		PartyIDs: []string{"testdept"},
+	}
+	_ = app.SendTextMessage(&to3, "send to party(parties)", false)
+
+	// send to tag(s)
+	to4 := workwx.Recipient{
+		TagIDs: []string{"testtag"},
+	}
+	_ = app.SendTextMessage(&to4, "send to tag(s)", false)
+
+	// send to chatid
+	to5 := workwx.Recipient{
+		ChatID: "testchat",
+	}
+	_ = app.SendTextMessage(&to5, "send to chatid", false)
+}
