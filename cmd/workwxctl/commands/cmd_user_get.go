30f12efb4bc5c2cb73d71a2006ca0340894071b5
Wang Xuerui <wangxuerui@qiniu.com>
2019-01-06 02:28:09 +0800

    feat: expose user/get in workwxctl

---
+++ cmd/workwxctl/commands/cmd_user_get.go
@@ -0,0 +1,24 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/urfave/cli"
+)
+
+func cmdUserGet(c *cli.Context) error {
+	cfg := mustGetConfig(c)
+	userid := c.Args().Get(0)
+
+	app := cfg.MakeWorkwxApp()
+	// TODO: failed requests currently panics
+	info, err := app.GetUser(userid)
+
+	if err != nil {
+		fmt.Printf("error = %+v\n", err)
+	} else {
+		fmt.Printf("user = %+v\n", info)
+	}
+
+	return err
+}
