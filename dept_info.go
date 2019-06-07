66be52068f981999cb9447f881b8024379a538b3
Wang Xuerui <wangxuerui@qiniu.com>
2019-06-07 15:08:26 +0800

    refactor: move dept_info.go to use sdkcodegen API call

--- dept_info.go
+++ dept_info.go
@@ -1,30 +1,27 @@
 package workwx
 
-const deptListEndpoint = "/cgi-bin/department/list"
-
 // ListAllDepts 获取全量组织架构。
 func (c *WorkwxApp) ListAllDepts() ([]*DeptInfo, error) {
-	return c.listDepts(false, 0)
+	resp, err := c.execDeptList(reqDeptList{
+		HaveID: false,
+		ID:     0,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Department, nil
 }
 
 // ListDepts 获取指定部门及其下的子部门。
 func (c *WorkwxApp) ListDepts(id int64) ([]*DeptInfo, error) {
-	return c.listDepts(true, id)
-}
-
-func (c *WorkwxApp) listDepts(haveID bool, id int64) ([]*DeptInfo, error) {
-	req := reqDeptList{
-		HaveID: haveID,
+	resp, err := c.execDeptList(reqDeptList{
+		HaveID: true,
 		ID:     id,
-	}
-
-	var resp respDeptList
-	err := c.executeQyapiGet(deptListEndpoint, req, &resp, true)
+	})
 	if err != nil {
-		// TODO: error_chain
 		return nil, err
 	}
 
-	obj := resp.Department
-	return obj, nil
+	return resp.Department, nil
 }
