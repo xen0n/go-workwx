7a7c51287e4ad4cec3e69ab5f95ea59d2325853f
Wang Xuerui <wangxuerui@qiniu.com>
2019-08-03 17:36:49 +0800

    chore: regenerate

--- apis.md.go
+++ apis.md.go
@@ -2,6 +2,7 @@
 
 package workwx
 
+// execGetAccessToken 获取access_token
 func (c *WorkwxApp) execGetAccessToken(req reqAccessToken) (respAccessToken, error) {
 	var resp respAccessToken
 	err := c.executeQyapiGet("/cgi-bin/gettoken", req, &resp, false)
@@ -12,6 +13,7 @@ func (c *WorkwxApp) execGetAccessToken(req reqAccessToken) (respAccessToken, err
 	return resp, nil
 }
 
+// execUserGet 读取成员
 func (c *WorkwxApp) execUserGet(req reqUserGet) (respUserGet, error) {
 	var resp respUserGet
 	err := c.executeQyapiGet("/cgi-bin/user/get", req, &resp, true)
@@ -22,6 +24,7 @@ func (c *WorkwxApp) execUserGet(req reqUserGet) (respUserGet, error) {
 	return resp, nil
 }
 
+// execDeptList 获取部门列表
 func (c *WorkwxApp) execDeptList(req reqDeptList) (respDeptList, error) {
 	var resp respDeptList
 	err := c.executeQyapiGet("/cgi-bin/department/list", req, &resp, true)
@@ -32,6 +35,7 @@ func (c *WorkwxApp) execDeptList(req reqDeptList) (respDeptList, error) {
 	return resp, nil
 }
 
+// execMessageSend 发送应用消息
 func (c *WorkwxApp) execMessageSend(req reqMessage) (respMessageSend, error) {
 	var resp respMessageSend
 	err := c.executeQyapiJSONPost("/cgi-bin/message/send", req, &resp, true)
@@ -42,6 +46,7 @@ func (c *WorkwxApp) execMessageSend(req reqMessage) (respMessageSend, error) {
 	return resp, nil
 }
 
+// execAppchatSend 应用推送消息
 func (c *WorkwxApp) execAppchatSend(req reqMessage) (respMessageSend, error) {
 	var resp respMessageSend
 	err := c.executeQyapiJSONPost("/cgi-bin/appchat/send", req, &resp, true)
