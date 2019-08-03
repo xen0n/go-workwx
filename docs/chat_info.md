beba7d12cf82bc5ba1838c612c99a11e691bf1f4
Wang Xuerui <wangxuerui@qiniu.com>
2019-08-03 17:43:18 +0800

    feat: add chat_info model

---
+++ docs/chat_info.md
@@ -0,0 +1,12 @@
+# 群聊
+
+## Models
+
+### `ChatInfo` 群聊信息
+
+Name|JSON|Type|Doc
+:---|:---|:---|:--
+`ChatID`|`chatid`|`string`| 群聊唯一标志
+`Name`|`name`|`string`|群聊名
+`OwnerUserID`|`owner`|`string`|群主id
+`MemberUserIDs`|`userlist`|`[]string`|群成员id列表
--- dummy_for_generate.go
+++ dummy_for_generate.go
@@ -1,5 +1,6 @@
 package workwx
 
 //go:generate go run ./internal/sdkcodegen ./docs/apis.md ./apis.md.go
+//go:generate go run ./internal/sdkcodegen ./docs/chat_info.md ./chat_info.md.go
 //go:generate go run ./internal/sdkcodegen ./docs/dept_info.md ./dept_info.md.go
 //go:generate go run ./internal/sdkcodegen ./docs/user_info.md ./user_info.md.go
