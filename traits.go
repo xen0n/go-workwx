66b21d468b2f5f2a8a0cbcc0c2b1e973e4dd451a
Wang Xuerui <wangxuerui@qiniu.com>
2019-10-02 17:56:26 +0800

    feat(chatbot): trait-ify auth params providing

--- traits.go
+++ traits.go
@@ -4,6 +4,11 @@ import (
 	"net/url"
 )
 
+// authenticator 提供 URL 鉴权参数的 trait
+type authenticator interface {
+	mutateURLWithAuthArgs(x /*mut*/ *url.URL)
+}
+
 // urlValuer 可转化为 url.Values 类型的 trait
 type urlValuer interface {
 	intoURLValues() url.Values
