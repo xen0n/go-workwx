c1b377cd191ee98eecb482c47dbc6f19e9854cd8
WANG Xuerui <wangxuerui@qiniu.com>
2020-03-12 18:07:23 +0800

    feat(lowlevel): add pkcs7 padding impl

--- internal/lowlevel/pkcs7.go
+++ internal/lowlevel/pkcs7.go
@@ -1,5 +1,16 @@
 package lowlevel
 
+func pkcs7Pad(x []byte) []byte {
+	numPadBytes := 32 - len(x)%32
+	padByte := byte(numPadBytes)
+	tmp := make([]byte, len(x)+numPadBytes)
+	copy(tmp, x)
+	for i := 0; i < numPadBytes; i++ {
+		tmp[len(x)+i] = padByte
+	}
+	return tmp
+}
+
 func pkcs7Unpad(x []byte) []byte {
 	// last byte is number of suffix bytes to remove
 	n := int(x[len(x)-1])
