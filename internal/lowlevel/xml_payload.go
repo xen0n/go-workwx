f2a25941917f495a795549d6d1a3ba53e4e58e8c
WANG Xuerui <wangxuerui@qiniu.com>
2020-03-12 19:20:04 +0800

    feat(lowlevel): implement response xml assembling

--- internal/lowlevel/xml_payload.go
+++ internal/lowlevel/xml_payload.go
@@ -1,9 +1,14 @@
 package lowlevel
 
 import (
+	"crypto/rand"
 	"encoding/xml"
 	"errors"
+	"io"
+	"math/big"
 	"net/url"
+	"strconv"
+	"time"
 )
 
 type xmlRxPayload struct {
@@ -12,9 +17,35 @@ type xmlRxPayload struct {
 	Encrypt    string `xml:"Encrypt"`
 }
 
+type cdataNode struct {
+	CData string `xml:",cdata"`
+}
+
+type xmlTxPayload struct {
+	XMLName      xml.Name  `xml:"xml"`
+	Encrypt      cdataNode `xml:"Encrypt"`
+	MsgSignature cdataNode `xml:"MsgSignature"`
+	Timestamp    int64     `xml:"Timestamp"`
+	Nonce        cdataNode `xml:"Nonce"`
+}
+
+type TimeSource interface {
+	GetCurrentTimestamp() time.Time
+}
+
+type DefaultTimeSource struct{}
+
+var _ TimeSource = DefaultTimeSource{}
+
+func (DefaultTimeSource) GetCurrentTimestamp() time.Time {
+	return time.Now()
+}
+
 type PayloadProcessor struct {
-	token     string
-	encryptor *WorkwxEncryptor
+	token         string
+	encryptor     *WorkwxEncryptor
+	entropySource io.Reader
+	timeSource    TimeSource
 }
 
 type MessagePayload struct {
@@ -34,8 +65,10 @@ func NewPayloadProcessor(
 	}
 
 	return &PayloadProcessor{
-		token:     token,
-		encryptor: enc,
+		token:         token,
+		encryptor:     enc,
+		entropySource: rand.Reader,         // TODO: support customization
+		timeSource:    DefaultTimeSource{}, // TODO: ditto
 	}, nil
 }
 
@@ -70,4 +103,59 @@ func (p *PayloadProcessor) HandleIncomingMsg(
 		Msg:        msg.Msg,
 		ReceiveID:  msg.ReceiveID,
 	}, nil
+}
+
+func (p *PayloadProcessor) MakeOutgoingMessage(msg []byte) ([]byte, error) {
+	workwxPayload := WorkwxPayload{
+		Msg:       msg,
+		ReceiveID: nil,
+	}
+	encryptedMsg, err := p.encryptor.Encrypt(&workwxPayload)
+	if err != nil {
+		return nil, err
+	}
+
+	ts := p.timeSource.GetCurrentTimestamp().Unix()
+	nonce, err := makeNonce(p.entropySource)
+	if err != nil {
+		return nil, err
+	}
+
+	msgSignature := makeDevMsgSignature(
+		p.token,
+		strconv.FormatInt(ts, 10),
+		nonce,
+		encryptedMsg,
+	)
+
+	payload := xmlTxPayload{
+		XMLName: xml.Name{},
+		Encrypt: cdataNode{
+			CData: encryptedMsg,
+		},
+		MsgSignature: cdataNode{
+			CData: msgSignature,
+		},
+		Timestamp: ts,
+		Nonce: cdataNode{
+			CData: nonce,
+		},
+	}
+
+	result, err := xml.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func makeNonce(entropySource io.Reader) (string, error) {
+	limit := big.NewInt(1)
+	limit = limit.Lsh(limit, 64)
+	n, err := rand.Int(entropySource, limit)
+	if err != nil {
+		return "", err
+	}
+	return n.String(), nil
 }
--- internal/lowlevel/xml_payload_test.go
+++ internal/lowlevel/xml_payload_test.go
@@ -1,6 +1,8 @@
 package lowlevel
 
 import (
+	"encoding/xml"
+	"fmt"
 	"net/url"
 	"testing"
 
@@ -10,12 +12,12 @@ import (
 func TestPayloadProcessor(t *testing.T) {
 	token := "kz7Yx62CH8SaLN"
 	encodingAESKey := "cD0d7jx4tYvVtzqrmh3Dm3QFCXe6f8SlHoMtMh3qQEP"
+	pr, err := NewPayloadProcessor(token, encodingAESKey)
+	if err != nil {
+		panic(err)
+	}
 
 	c.Convey("完整的请求应该能正常解出内容", t, func() {
-		pr, err := NewPayloadProcessor(token, encodingAESKey)
-		c.So(err, c.ShouldBeNil)
-		c.So(pr, c.ShouldNotBeNil)
-
 		s := "http://test.example.com/?msg_signature=f265ae551b1932727204c3d707628d01376a6940&timestamp=1583995625&nonce=1584392382"
 		body := "<xml><ToUserName><![CDATA[ww6a112864f8022910]]></ToUserName><Encrypt><![CDATA[EUCt7xMcNiyASzZj0Hjc5yDjFQrCum6AfQ3ntHiUzjGQ51xieKmbvtrZ40/EcB2W/W8yH0n4Lqx48gJl/T9HD/R309I0P/r5pIZucK3lyEn48FYMr4YdE0QdL2jIJ3xkcXUr6uzefzCxG6lMvwpAJaOyVCzN7sRRw47njfxy5EIqU6R9ZBhlTzfdnhhOhK/nTwzrZX3SoGlXFA9OBeZ6ru1NWpXFk76x9DUMe0lcxPPiUqK8ctnQcYXSGUHVqC6DfG7E7mab0OmruNN8cBZY5d3dYOBA4OgaH55Q0AJmUpdT8vNiXpXx+6TxT3TIjySXpDrHVyrsb772aYywgg/Nu4kUmGkALwFZlzhjNegR7wDwb9lr4ERXsSSS8JZ8lbBmaQ3F2Tq584xoPj5rIhXAF734ynm4no1g+SdHiNqR328=]]></Encrypt><AgentID><![CDATA[1000002]]></AgentID></xml>"
 		u, err := url.Parse(s)
@@ -31,5 +33,40 @@ func TestPayloadProcessor(t *testing.T) {
 			ReceiveID:  []byte("ww6a112864f8022910"),
 		}
 		c.So(payload, c.ShouldResemble, expected)
+	})
+
+	c.Convey("被动响应包应该能正常组装", t, func() {
+		originalMsg := []byte("foobarbazquux123321")
+
+		out, err := pr.MakeOutgoingMessage(originalMsg)
+		c.So(err, c.ShouldBeNil)
+
+		// 重新解出数据
+		var outDeserialized struct {
+			Encrypt      string `xml:"Encrypt"`
+			MsgSignature string `xml:"MsgSignature"`
+			Timestamp    int64  `xml:"Timestamp"`
+			Nonce        string `xml:"Nonce"`
+		}
+
+		err = xml.Unmarshal(out, &outDeserialized)
+		c.So(err, c.ShouldBeNil)
+
+		// 做一个假的 URL
+		s := fmt.Sprintf(
+			"http://a.b/?msg_signature=%s&timestamp=%d&nonce=%s",
+			outDeserialized.MsgSignature,
+			outDeserialized.Timestamp,
+			outDeserialized.Nonce,
+		)
+		u, err := url.Parse(s)
+		c.So(err, c.ShouldBeNil)
+
+		// 亏得 Go encoding 库都对错误的字段名很宽恕
+		// 只有 Encrypt 元素有意义，不过正好 ;-)
+		rtt, err := pr.HandleIncomingMsg(u, out)
+		c.So(err, c.ShouldBeNil)
+		c.So(rtt.Msg, c.ShouldResemble, originalMsg)
+		c.So(rtt.ReceiveID, c.ShouldBeEmpty)
 	})
 }
