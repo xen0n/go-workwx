447c4580f5404a0ca69d090741e4c50082d9e52f
WANG Xuerui <wangxuerui@qiniu.com>
2020-03-12 13:26:34 +0800

    feat(lowlevel): add signature verification

--- lowlevel/signature_test.go
+++ lowlevel/signature_test.go
@@ -1,6 +1,7 @@
 package lowlevel
 
 import (
+	"net/url"
 	"testing"
 
 	c "github.com/smartystreets/goconvey/convey"
@@ -17,5 +18,27 @@ func TestMakeDevMsgSignature(t *testing.T) {
 		devMsgSignature := makeDevMsgSignature(values...)
 		msgSignature := "1ba3cb09c0d2c2b3ed6900d37f91a6efae6cb011"
 		c.So(devMsgSignature, c.ShouldEqual, msgSignature)
+	})
+}
+
+func TestVerifyURLSignature(t *testing.T) {
+	token := "kjr2TKI8umCBfVF3wAHk8JiPwma5VBme"
+
+	c.Convey("带有完好签名的 URL 应该能通过校验", t, func() {
+		s := "http://test.example.com/?echostr=6KmUQuPVu7UhjyVqRdbo5SfcRqaHvbUlKSHFvBV2ZuR6TIlKsygcfeSd1GDplg1C5KSKr6UPHCaC%2FnIX3ZNt9w%3D%3D&msg_signature=1ba3cb09c0d2c2b3ed6900d37f91a6efae6cb011&timestamp=1583940690&nonce=VHh7ymSeb0jc4lSb"
+		u, err := url.Parse(s)
+		c.So(err, c.ShouldBeNil)
+
+		ok := VerifyURLSignature(token, u)
+		c.So(ok, c.ShouldBeTrue)
+	})
+
+	c.Convey("签名受损的 URL 应该通不过校验", t, func() {
+		s := "http://test.example.com/?echostr=6KmUQuPVu7UhjyVqRdbo5SfcRqaHvbUlKSHFvBV2ZuR6TIlKsygcfeSd1GDplg1C5KSKr6UPHCaC%2FnIX3ZNt9w%3D%3D&msg_signature=1ba3cb09c0d2c2b3ed6900d37f91a6efae6cb010&timestamp=1583940690&nonce=VHh7ymSeb0jc4lSb"
+		u, err := url.Parse(s)
+		c.So(err, c.ShouldBeNil)
+
+		ok := VerifyURLSignature(token, u)
+		c.So(ok, c.ShouldBeFalse)
 	})
 }
