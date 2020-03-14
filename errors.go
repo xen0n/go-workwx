2ab83f6fcf36657d4f871c297927088dd1041135
WANG Xuerui <wangxuerui@qiniu.com>
2020-03-14 13:22:18 +0800

    feat(errors): split errcodes into own package & tweak doc comment

--- errors.go
+++ errors.go
@@ -2,6 +2,8 @@ package workwx
 
 import (
 	"fmt"
+
+	"github.com/xen0n/go-workwx/errcodes"
 )
 
 // WorkwxClientError 企业微信客户端 SDK 的响应错误
@@ -9,7 +11,7 @@ type WorkwxClientError struct {
 	// Code 错误码，0表示成功，非0表示调用失败。
 	//
 	// 开发者需根据errcode是否为0判断是否调用成功(errcode意义请见全局错误码)。
-	Code ErrCode
+	Code errcodes.ErrCode
 	// Msg 错误信息，调用失败会有相关的错误信息返回。
 	//
 	// 仅作参考，后续可能会有变动，因此不可作为是否调用成功的判据。
