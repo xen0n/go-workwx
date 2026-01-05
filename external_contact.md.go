xen0n/go-workwx

commit 6b11302ad5f454329654499575c63c9a0bf0397c

feat: KfMsg添加语音消息类型支持

external_contact.md.go
@@ -344,6 +344,11 @@ type Text struct {
 	Content string `json:"content"`
 }
 
+type Voice struct {
+	// MediaID 语音文件的media_id
+	MediaID string `json:"media_id"`
+}
+
 // Image 图片类型消息
 type Image struct {
 	// MediaID 图片的media_id

kf.md.go
@@ -56,6 +56,8 @@ type KfMsg struct {
 	MsgType MessageType `json:"msgtype"`
 	// Text 文本消息
 	Text Text `json:"text,omitempty"`
+	// Voice 语音消息
+	Voice Voice `json:"voice,omitempty"`
 	// Image 图片消息
 	Image Image `json:"image,omitempty"`
 	// Link 链接消息

commit 25ce7cbfd4f39b2404dab5bc40c2be996872d458

fix: add missing docs for Kf Voice message types
Fixes: 6b11302ad5f4 ("feat: KfMsg添加语音消息类型支持")

external_contact.md.go
@@ -344,6 +344,7 @@ type Text struct {
 	Content string `json:"content"`
 }
 
+// Voice 语音消息
 type Voice struct {
 	// MediaID 语音文件的media_id
 	MediaID string `json:"media_id"`